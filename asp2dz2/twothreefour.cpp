da49f8ab3fa5c6a9f3b37e793d42b24f292940f9
Milan Prokić <prokic.milan@gmail.com>
2017-11-18 20:36:33 +0100

    Zavrseno ubacivanje cvora

--- asp2dz2/twothreefour.cpp
+++ asp2dz2/twothreefour.cpp
@@ -39,73 +39,6 @@ const Process* TwoThreeFour::findKeyExec(const long time) const {
 	}
 }
 
-/*void TwoThreeFour::addKey(Process *p) {
-	TreeNode *curr = nullptr;
-	TreeNode *prev = nullptr;
-	
-	//ako je stablo prazno, proces se umece u koren
-	if (root == nullptr) {
-		root = new TreeNode();
-		root->addKey(p);
-	}
-	else {
-		curr = root;
-		//pronalazenje odgovarajuceg lista u koji treba umetnuti proces
-		while (curr != nullptr) {
-			prev = curr;
-			curr = curr->getNextWait(p->getWaitingTime());
-		}
-		//broj kljuceva u listu
-		auto keys = prev->getKeys();
-		//da li ovo moze sa keys.size()?
-		//int cnt = std::count_if(keys.begin(), keys.end(), [](Process *ptr) { return ptr != nullptr; });
-		int cnt = keys.size();
-		//ima mesta za dodavanje novog kljuca
-		if (cnt < 3) {
-			prev->addKey(p);
-		}
-		//nema mesta, cepanje cvora
-		else {
-			Process *extra = keys.at(1);
-			TreeNode *left = new TreeNode();
-			TreeNode *right = new TreeNode();
-			bool addLeft = false;
-
-			left->setParent(prev->getParent());
-			right->setParent(prev->getParent());
-			left->addKey(keys.at(0));
-			right->addKey(keys.at(2));
-			//novi kljuc se dodaje u onaj cvor cijem je kljucu blizi
-			addLeft = abs(keys.at(0)->getWaitingTime() - p->getWaitingTime()) < abs(keys.at(2)->getWaitingTime() - p->getWaitingTime()) ? true : false;
-			if (addLeft) {
-				left->addKey(p);
-			}
-			else {
-				right->addKey(p);
-			}
-			curr = prev->getParent();
-			while (curr != nullptr) {
-				keys = curr->getKeys();
-				cnt = keys.size();
-				//cnt = std::count_if(keys.begin(), keys.end(), [](Process *ptr) { return ptr != nullptr; });
-				//cvor otac ima dovoljno mesta za visak
-				if (cnt < 3) {
-					curr->addKey(extra);
-					int newPos = std::find(keys.begin(), keys.end(), extra) - keys.begin();
-					int disp = keys.size() - 1 - newPos;
-					curr->moveSons(disp);
-					curr->setSon(newPos, left);
-					curr->setSon(newPos + 1, right);
-					break;
-				}
-				//cvor otac nema dovoljno mesta, mora da se cepa
-				else {
-
-				}
-			}
-		}
-	}
-}*/
 void TwoThreeFour::addKey(Process* p) {
 	TreeNode *curr = nullptr;
 	TreeNode *prevDel = nullptr;
@@ -124,8 +57,7 @@ void TwoThreeFour::addKey(Process* p) {
 			if (cnt == 3) {
 				TreeNode *left = new TreeNode();
 				TreeNode *right = new TreeNode();
-				TreeNode *parent = nullptr;
-				Process *extra = keys.at(1);
+				Process *extra = keys[1];
 				left->addKey(keys[0]);
 				right->addKey(keys[2]);
 				left->setSon(0, sons[0]);
@@ -133,15 +65,15 @@ void TwoThreeFour::addKey(Process* p) {
 				right->setSon(0, sons[2]);
 				right->setSon(1, sons[3]);
 				if (curr != root) {
-					auto parentKeys = curr->getParent()->getKeys();
-					left->setParent(curr->getParent());
-					right->setParent(curr->getParent());
-					curr->getParent()->addKey(extra);
-					int pos = curr->getParent()->find(extra);
-					int disp = std::count_if(keys.begin(), keys.end(), [](Process *ptr) { return ptr != nullptr; }) - pos - 2;
-					curr->getParent()->moveSons(disp);
-					curr->getParent()->setSon(pos, left);
-					curr->getParent()->setSon(pos + 1, right);
+					auto parentKeys = prev->getKeys();
+					left->setParent(prev);
+					right->setParent(prev);
+					prev->addKey(extra);
+					int pos = prev->find(extra);
+					int disp = std::count_if(parentKeys.begin(), parentKeys.end(), [](Process *ptr) { return ptr != nullptr; }) - pos - 1;
+					prev->moveSons(disp);
+					prev->setSon(pos, left);
+					prev->setSon(pos + 1, right);
 					bool addLeft = abs(keys[0]->getWaitingTime() - p->getWaitingTime()) < abs(keys[2]->getWaitingTime() - p->getWaitingTime()) ? true : false;
 					prevDel = curr;
 					curr = addLeft ? left : right;
